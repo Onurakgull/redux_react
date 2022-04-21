b54c5906842dfcfe49f8c0c94c035c31c2d89bf5
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2022-04-21 13:21:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Index from "./component";
 import Data from "./component/Data";
 import HookSayac from "./component/HookSayac";
 
+
+export const lightTheme={
+  type: "light",
+  body: "#E2E2E2",
+  text: "#363537",
+  toggleBorder: "#FFF",
+  gradient: "lineer-gradient(#39598A,#79D7ED)"
+};
+export const darkTheme={
+  type: "dark",
+  body: "#363537",
+  text: "#fafafa",
+  toggleBorder: "#6bb096",
+  gradient: "lineer-gradient(#091236,#1E215D)"
+}
+const ThemeContext= React.createContext(null); 
+
 function App() {
+  const [theme, setTheme]=useState(darkTheme)
+
+  const handleThemeChange=()=>{
+    if(theme.type==="dark"){
+      setTheme(lightTheme);
+    }else{
+      setTheme(darkTheme);
+    }
+  }
   return (
     <>
-        <Index/>
-        <HookSayac/>
-        <Data/>
+         <button onClick={()=>handleThemeChange()}>{theme.type==="dark" ? 'Light Theme' : 'Dark Theme'}</button>
+         <ThemeContext.Provider value={theme}>
+          <Kare/>
+        </ThemeContext.Provider>
+            <Index/>
+            <HookSayac/>
+            <Data/>
+     
     </>
   );
 }
+const Kare = () =>{
+    const theme = useContext(ThemeContext)
+    return(
+      <div style={ {color: `${theme.gradient}`}}>
+        Temam: {theme.type}
+        <h2>Tema light</h2>
+      </div>
+    );
+};
 
 export default App;
