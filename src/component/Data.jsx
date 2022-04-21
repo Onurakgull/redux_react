c5727139da1ee2d9ca9642b84d19cf37bf1684eb
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2022-04-21 10:41:53 +0300

    Create Data.jsx

---
+++ src/component/Data.jsx
@@ -0,0 +1,28 @@
+import React,{useEffect, useState} from "react";
+import axios from 'axios'
+
+const Data = () => {
+    const [posts, setPosts] =useState([]);
+    const fetchPost = async() => {
+        const {data} = await axios.get("https://jsonplaceholder.typicode.com/posts");
+        setPosts(data)
+    }
+
+    useEffect(() => {
+        fetchPost()
+    },[]);
+
+    return(
+        <div>
+            <ul>
+                {
+                    posts.map(post =>(
+                        <li key={post.id}>{post.id}--{post.title}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
+}
+
+export default Data;
